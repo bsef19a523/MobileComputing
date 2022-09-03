bb731253ea7362939a5f3b4602b0bcf3e5cd4494
bsef19a523 <bsef19a523@pucit.edu.pk>
2022-09-03 11:46:57 +0500

    Update DBHelper.java

--- Quran/app/src/main/java/haqnawaz/org/navigationdrawer/DBHelper.java
+++ Quran/app/src/main/java/haqnawaz/org/navigationdrawer/DBHelper.java
@@ -21,7 +21,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public DBHelper(@Nullable Context context) {
-        super(context, "Quran.db", null, 1);
+        super(context, "data.sqlite", null, 1);
     }
 
     @Override
